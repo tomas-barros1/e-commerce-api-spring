2effa16e440b24f66f09d361585197ea9c519518
tomas-barros1 <tomasabbarros3@gmail.com>
2025-01-27 10:19:11 -0300

    autenticação funcionando

--- src/main/java/ecomerce/api/controllers/AuthController.java
+++ src/main/java/ecomerce/api/controllers/AuthController.java
@@ -42,7 +42,7 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
-        if (this.userRepository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+        if (this.userRepository.findByEmail(data.email()).isPresent()) return ResponseEntity.badRequest().build();
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
         User newUser = new User(null, data.name(), data.email(), data.phone(), encryptedPassword, data.role());
---
+++ src/main/java/ecomerce/api/infra/security/UserDetailsServicesImp.java
@@ -0,0 +1,22 @@
+package ecomerce.api.infra.security;
+
+import ecomerce.api.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServicesImp implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServicesImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário de email: " + username + " não encontrado"));
+    }
+}
